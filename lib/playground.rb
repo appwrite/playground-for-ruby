5e485d23c975a1cf8930ccae57309df6229c9d59
Avinash Upadhyaya <avinashupadhya99@gmail.com>
2021-10-06 14:32:23 +0530

    refactor: use functions for each operation

--- lib/playground.rb
+++ lib/playground.rb
@@ -3,28 +3,30 @@
 require "appwrite"
 require "dotenv/load"
 
-client = Appwrite::Client.new
+$client = Appwrite::Client.new
 
 appwrite_endpoint = ENV["APPWRITE_ENDPOINT"]
 appwrite_project = ENV["APPWRITE_PROJECT_ID"]
 appwrite_api_key = ENV["APPWRITE_API_KEY"]
 
-client
+$client
   .set_endpoint(appwrite_endpoint) # Your API Endpoint
   .set_project(appwrite_project) # Your project ID
   .set_key(appwrite_api_key) # Your secret API key
 
-database = Appwrite::Database.new(client)
+def create_collection
+  database = Appwrite::Database.new($client)
+  puts("Running Create Collection API")
 
-response = database.create_collection(
-  name: "Movies", # Collection Name
-  read: [], # Read permissions
-  write: [], # Write permissions
-  rules: [
-    { label: "Name", key: "name", type: "text", default: "Empty Name", required: true, array: false },
-    { label: "release_year", key: "release_year", type: "numeric", default: 1970, required: true, array: false }
-  ]
-)
+  response = database.create_collection(
+    name: "Movies", read: [], write: [],
+    rules: [
+      { label: "Name", key: "name", type: "text", default: "Empty Name", required: true, array: false },
+      { label: "release_year", key: "release_year", type: "numeric", default: 1970, required: true, array: false }
+    ]
+  )
 
-puts("Running Create Collection API")
-puts response
+  puts response
+end
+
+create_collection
